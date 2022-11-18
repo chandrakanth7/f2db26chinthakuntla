a6305ba79e855c4b899beded29447819d97a1567
chandrakanth7 <42292161+chandrakanth7@users.noreply.github.com>
2022-11-17 18:56:18 -0600

    worked on lab 13

--- app.js
+++ app.js
@@ -7,6 +7,22 @@ var mongodb = require('mongodb');
 var logger = require('morgan');
 var chickenDish = require('./models/chickenDish');
 
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    Account.findOne({ username: username }, function (err, user) {
+  if (err) { return done(err); }
+  if (!user) {
+    return done(null, false, { message: 'Incorrect username.' });
+  }
+  if (!user.validPassword(password)) {
+    return done(null, false, { message: 'Incorrect password.' });
+  }
+    return done(null, user);
+  });
+  }))
+
 require('dotenv').config();
 const connectionString = process.env.MONGO_CON
 mongoose = require('mongoose');
@@ -37,6 +53,14 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(require('express-session')({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false
+ }));
+ app.use(passport.initialize());
+ app.use(passport.session());
+ 
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
@@ -50,6 +74,10 @@ app.use('/chickenDish',createRouter);
 app.use('/chickenDish',updateRouter);
 app.use('/chickenDish',deleteRouter);
 
+var Account =require('./models/account');
+passport.use(new LocalStrategy(Account.authenticate()));
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser());
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
